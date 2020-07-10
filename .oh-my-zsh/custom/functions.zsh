7922ad7e9af63debdb21e1c4e50ba5370556e458
Matthew Schindler <mschindler1@2u.com>
2020-07-10 10:35:47 -0400

    [UPDATE] * adding in scripts for auto-login  * VPN  * 2u-login

--- .oh-my-zsh/custom/functions.zsh
+++ .oh-my-zsh/custom/functions.zsh
@@ -15,3 +15,27 @@ function _kcsc_complete {
     COMPREPLY=($list)
     return 0
 }
+
+# 2U VPN Login
+2u-vpn () {
+        local command="${1:-s}"
+        local -r _vpn_bin='/opt/cisco/anyconnect/bin/vpn'
+        local -r _vpn_net='2U Corp Network'
+        case "$command" in
+                ('s') echo -e 'Checking current VPN status...\n'
+                        eval "${_vpn_bin} -s status" ;;
+                ('c') echo -e 'Connecting to VPN...\n'
+                        _vpn_autoconnect ;;
+                ('d') echo -e 'Disconnecting from VPN...\n'
+                        eval "${_vpn_bin} -s disconnect" ;;
+                (*) echo "Invalid option '${command}' ([(s)tatus]|(c)onnect|(d)isconnect)"
+                        return 1 ;;
+        esac
+}
+
+# calls login scripts
+login () {
+        2u-vpn c
+        sleep 5
+        /usr/local/bin/_login
+}
