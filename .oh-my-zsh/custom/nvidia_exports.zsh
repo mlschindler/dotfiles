ca3a366091bbae063242d5eb8ec11d703eacc623
Matthew Schindler <mschindler@2u.com>
2020-11-18 17:13:51 -0500

    adding nvidia exports

---
+++ .oh-my-zsh/custom/nvidia_exports.zsh
@@ -0,0 +1,8 @@
+# Vault Exports
+export VAULT_STG="https://stg.vault.nvidia.com"
+export VAULT_PROD="https://prod.vault.nvidia.com"
+export VAULT_NAMESPACE="nvcloudsec"
+
+# AWS Exports
+export NVCLOUDSEC_DEV="arn:aws:iam::703088442575:role/AWSOS-AD-Engineer"
+export NVCLOUDSEC_PROD="arn:aws:iam::515825426174:role/AWSOS-AD-Engineer"
