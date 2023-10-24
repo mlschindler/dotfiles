3270ee044519b2922d260e9905ff3444672e3155
Matthew Schindler <matt.lowery.schindler@gmail.com>
2023-10-24 15:06:36 -0400

    adding vpn alias for austin

--- .oh-my-zsh/custom/alias.zsh
+++ .oh-my-zsh/custom/alias.zsh
@@ -20,3 +20,5 @@ alias config='/usr/bin/git --git-dir=/Users/mschindler/.cfg/ --work-tree=/Users/
 
 # NVIDIA specific aliases
 alias aws_login='nvsec configure user && nvsec awsos get-creds --aws-profile default'
+alias vpn_durham='sudo openconnect-sso -s ngvpn04.vpn.nvidia.com -g SAML'
+alias vpn_austin='sudo openconnect ngvpn03.vpn.nvidia.com/SAML-EXT'
